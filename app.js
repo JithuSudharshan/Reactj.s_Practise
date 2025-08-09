8f200ad6c5e06016399928b88f67f6debdbc4b70
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-08-10 00:40:58 +0530

    React app settup

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
+    [React.createElement("div", { id: "child 1" },
         [React.createElement("h1", {}, "hi i'm H1 tag"),
         React.createElement("h2", {}, "hi i'm H2 tag")
         ]
-    ),React.createElement("div", { id: "child" },
+    ),React.createElement("div", { id: "child 2" },
         [React.createElement("h1", {}, "hi i'm H1 tag"),
         React.createElement("h2", {}, "hi i'm H2 tag")
         ]
